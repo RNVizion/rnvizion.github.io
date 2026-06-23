b92ea8ccbc445458617eefc994856cb2da114151
Christian Smith <18348535+RNVizion@users.noreply.github.com>
2026-06-22 23:57:52 -0400

    Delete agent.sh

--- agent.sh
+++
@@ -1,7 +0,0 @@
-# (run inside the rnvizion.github.io Codespace)
-cd /workspaces
-git clone https://github.com/RNVizion/publishing-agent.git
-cd publishing-agent
-pip install -q -r requirements.txt 
-# non-destructive proof: dry-run an existing post
-python agent.py "Publish the post at blog/squish/ as a dry run."
