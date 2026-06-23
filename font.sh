259c60a0e27fd7c7adbdb032dd1738fed29afcd7
Christian Smith <18348535+RNVizion@users.noreply.github.com>
2026-06-22 23:42:46 -0400

    Update font.sh

--- font.sh
+++ font.sh
@@ -1,3 +1,4 @@
 mkdir -p assets/fonts
 curl -L -o assets/fonts/BricolageGrotesque.ttf "https://raw.githubusercontent.com/google/fonts/main/ofl/bricolagegrotesque/BricolageGrotesque%5Bopsz%2Cwdth%2Cwght%5D.ttf"
 curl -L -o assets/fonts/JetBrainsMono.ttf "https://raw.githubusercontent.com/google/fonts/main/ofl/jetbrainsmono/JetBrainsMono%5Bwght%5D.ttf"
+curl -L -o assets/fonts/Montserrat.ttf "https://raw.githubusercontent.com/google/fonts/main/ofl/montserrat/Montserrat%5Bwght%5D.ttf"
